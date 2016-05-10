36156cbacf7ffed51f474bcef01e17ece789f7a6
Andrew Benz <andorbal@gmail.com>
2016-05-10 17:57:04 -0500

    Add hour and month tests

--- test/ex_cron/parser_test.exs
+++ test/ex_cron/parser_test.exs
@@ -28,4 +28,56 @@ defmodule ExCron.ParserTest do
   test "parse multiple minute ranges" do
     assert %Cron{minutes: [6,10,11,12,15,16]} = ExCron.Parser.parse "10-12,6,15-16 * * * *"
   end
+
+  ### Hour tests
+  test "parse single hour" do
+    assert %Cron{hours: [7]} = ExCron.Parser.parse "* 7 * * *"
+  end
+
+  test "parse multiple hours" do
+    assert %Cron{hours: [6, 14]} = ExCron.Parser.parse "* 14,6 * * *"
+  end
+
+  test "parse hour wildcard" do
+    hours = 0..23 |> Enum.to_list
+    assert %Cron{hours: ^hours} = ExCron.Parser.parse "* * * * *"
+  end
+
+  test "parse hours with fractional wildcard" do
+    assert %Cron{hours: [0,6,12,18]} = ExCron.Parser.parse "* */6 * * *"
+  end
+
+  test "parse hour range" do
+    assert %Cron{hours: [10,11,12]} = ExCron.Parser.parse "* 10-12 * * *"
+  end
+
+  test "parse multiple hour ranges" do
+    assert %Cron{hours: [6,10,11,12,15,16]} = ExCron.Parser.parse "* 10-12,6,15-16 * * *"
+  end
+
+  ### Month tests
+  test "parse single month" do
+    assert %Cron{months: [7]} = ExCron.Parser.parse "* * * 7 *"
+  end
+
+  test "parse multiple months" do
+    assert %Cron{months: [6, 12]} = ExCron.Parser.parse "* * * 12,6 *"
+  end
+
+  test "parse month wildcard" do
+    months = 1..12 |> Enum.to_list
+    assert %Cron{months: ^months} = ExCron.Parser.parse "* * * * *"
+  end
+
+  test "parse months with fractional wildcard" do
+    assert %Cron{months: [3,6,9,12]} = ExCron.Parser.parse "* * * */3 *"
+  end
+
+  test "parse month range" do
+    assert %Cron{months: [10,11,12]} = ExCron.Parser.parse "* * * 10-12 *"
+  end
+
+  test "parse multiple month ranges" do
+    assert %Cron{months: [3,4,6,10,11,12]} = ExCron.Parser.parse "* * * 10-12,6,3,4 *"
+  end
 end
