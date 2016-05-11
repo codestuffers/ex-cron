c16fa7b6f823ec7e4ad1879cd9c46fa9c1964993
Andrew Benz <andorbal@gmail.com>
2016-05-10 22:48:42 -0500

    Add a few more tests around the within function

--- test/ex_cron_test.exs
+++ test/ex_cron_test.exs
@@ -5,4 +5,14 @@ defmodule ExCronTest do
   test "next gets the next instance" do
     assert {{2016,5,10},{23,9,0}} = ExCron.next "9 * * * *", {{2016,5,10},{22,10,12}}
   end
+
+  test "within gets a list of insteances within the date range" do
+    dates = ExCron.within "10 20 * OCT-DEC TUE-THU", {{2016,5,10},{23,9,0}}, {{2016,10,8},{6,30,12}}
+    assert dates == [{{2016,10,4},{20,10,0}},{{2016,10,5},{20,10,0}},{{2016,10,6},{20,10,0}}]
+  end
+
+  test "within gets a large number of results" do
+    dates = ExCron.within "* * * * *", {{2016,5,10},{23,9,0}}, {{2016,10,8},{6,30,12}}
+    assert Enum.count(dates) == 186202
+  end
 end
